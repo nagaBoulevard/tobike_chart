56637cfd0b2560cab32cea5899511afcfada1433
gilo <gilotto.is@gmail.com>
2017-06-07 16:46:27 +0200

    :rocket: parser ok!

--- pipeline.sh
+++ pipeline.sh
@@ -2,12 +2,19 @@ DATA=./data/
 mkdir -p $DATA
 
 # fetch the TOBIKE data
-wget http://www.tobike.it/frmLeStazioni.aspx -O $DATA/tobike_data.aspx;
+wget http://www.tobike.it/frmLeStazioni.aspx -O $DATA/tobike.aspx;
 
-# select the important data
-cat $DATA/tobike_data.aspx | tr -d '\000' \
+cat $DATA/tobike.aspx | tr -d '\000' \
 | grep -o "{RefreshMap(.*}" | sed 's/{RefreshMap(\(.*\))}/\1/' \
-| sed "s|','|\n|g" | sed "s|'||g" > $DATA/tobike_data.parsed
+| sed "s|','|\n|g" | sed "s|'||g" > $DATA/tobike.parsed
+
+
+cat $DATA/tobike.parsed \
+| head -n 9 | grep -v "<strong>" | sed 's/://g' | tr " " "_" \
+| iconv -c -f utf-8 -t ascii \
+| ruby -e 'puts readlines.map{|x| x.split "|" }.transpose.map{|x|x*" "}' \
+| tr -s " " "\t" | cut -f1,6,7  | head -n -8 > $DATA/tobike.stations
+
 
 
 
